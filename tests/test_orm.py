d2c6a6852616069c36d6f5c581e9198b4a7f1a27
Radman Shahbazkhan <radikahn@gmail.com>
2025-05-04 14:33:11 -0700

    Init Unittesting

---
+++ tests/test_orm.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestOrmFunction(unittest.TestCase):
+
+    def test_table_creation(self):
+        pass
+
+    
+    def test_table_add(self):
+        pass
+
+
+    def test_table_update(self):
+        pass
+
+
+    def test_table_query(self):
+        pass
---
+++ tests/test_user_obj.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestUserObjects(unittest.Testcase):
+
+    def Test_User_Create(self):
+        pass
+
+
+    def Test_User_Username_Change(self):
+        pass
+
+
+    def Test_Add_Attributes(self):
+        pass
